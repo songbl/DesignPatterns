096e04df727d47f001ee33fe2d2240c815648fcd
songbl <791243928@qq.com>
2018-12-13 16:06:56 +0800

    策略模式再理解

--- src/singleinstance/SingleInstance.java
+++ src/singleinstance/SingleInstance.java
@@ -11,7 +11,7 @@ public class SingleInstance {
     /**
      * 如果synchronized加在方法上，每次都要走，降低性能
      * 而且只有第一次的时候，确实有用，累赘
-     * 1）因为A和B是第一批调用者，当它们进入静态工厂方法时，instance变量是null。因此它们几乎同时到达 位置2。
+     * 1）因为A和B是第一批调用者，当它们进入静态工厂方法时，instance变量是null。因此它们几乎同时到达===位置2====。
      * 2)  假设A线程先进入 synchronized (SingleInstance.class)，到达位置3，这时由于同步机制，线程B无法到达位置3，只能在位置2等待。
      * 3）singleInstance = new SingleInstance()语句，使得instance引用指向一个对象。此时线程B还在位置2上等待。
      * 4）线程A退出synchronized (instance)，singleInstance，退出静态工厂方法。
--- src/strategypattern/FlyBehavior.java
+++ src/strategypattern/FlyBehavior.java
@@ -2,6 +2,7 @@ package strategypattern;
 
 /**
  * 飞行接口（不同鸭子类型，不同的飞行方式）
+ *  ===这就是哪个算法族=====实现这个接口的类  =======相互替换
  */
 public interface FlyBehavior {
     public void fly();
